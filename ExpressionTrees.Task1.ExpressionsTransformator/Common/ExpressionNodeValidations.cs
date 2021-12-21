8a6cabbde58cc37578fa9a46dc5ab63ffe7d89ac
Arnas <arnas.vaiceliunas@gmail.com>
2021-12-21 16:16:56 +0200

    Validation and node checks refactoring

---
+++ ExpressionTrees.Task1.ExpressionsTransformator/Common/ExpressionNodeValidations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace ExpressionTrees.Task1.ExpressionsTransformer.Common
+{
+    public class ExpressionNodeValidations
+    {
+        public static bool IsInt(Expression o)
+        {
+            return o.Type == typeof(int);
+        }
+
+        public static bool Is1(int i)
+        {
+            return i == 1;
+        }
+
+        public static bool IsNull(Expression o)
+        {
+            return o == null;
+        }
+    }
+}
